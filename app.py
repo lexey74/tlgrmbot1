87ba5f7d270c74330e28df00916182fde22cc410
lexey74 <otherman2011@inbox.ru>
2024-05-23 16:44:44 +0300

    Добавил проверку валидных IP адресов серверов Telegram

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import os
 from contextlib import asynccontextmanager
 from logging.config import dictConfig
 
@@ -7,6 +8,7 @@ from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 from telegram import Update
 from telegram.error import TelegramError
+from dotenv import load_dotenv
 
 from config import LOGGER_CONFIG, TELEGRAM_BOT_TOKEN, WEBHOOK_URL
 
@@ -16,6 +18,18 @@ from scheduler_service import ScheduleService
 
 logger = logging.getLogger('app')
 
+# Загрузка переменных окружения из .env файла
+load_dotenv()
+
+# Получение списка доверенных IP-адресов из переменных окружения
+TRUSTED_IP_RANGES = os.getenv("TRUSTED_IP_RANGES", "").split(",")
+
+def is_trusted_ip(ip: str) -> bool:
+    ip_addr = ipaddress.ip_address(ip)
+    for ip_range in TRUSTED_IP_RANGES:
+        if ip_addr in ipaddress.ip_network(ip_range.strip()):
+            return True
+    return False
 
 class CustomFastAPI(FastAPI):
     tg: 'TelegramService'
@@ -49,6 +63,13 @@ def get_app() -> CustomFastAPI:
 
 @app.post(f'/{TELEGRAM_BOT_TOKEN}')
 async def webhook(request: Request, app: CustomFastAPI = Depends(get_app)):
+    # Test IP address
+    client_host = request.client.host
+    if not is_trusted_ip(client_host):
+        logger.warning(f'Недоверенный IP-адрес: {client_host}')
+        raise HTTPException(status_code=403, detail="Forbidden")
+
+    
     if request.headers.get('Content-Type') == 'application/json':
         logger.info(f'Получен Webhook запрос: {await request.body()}')
         update = Update.de_json(await request.json(), app.tg.bot_app.bot)
